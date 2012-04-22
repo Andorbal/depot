866d45c5c69c4083123d9efabc79f8568619edfa
Andrew Benz <andorbal@gmail.com>
2012-04-22 08:29:19 -0500

    Modified the cart so that multiple line items of the same type are condensed into one line item with a quantity.

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,5 +1,5 @@
 class LineItem < ActiveRecord::Base
-  attr_accessible :cart_id, :product_id, :cart, :product
+  attr_accessible :cart_id, :product_id, :cart, :product, :quantity
   belongs_to :product
   belongs_to :cart
 end
