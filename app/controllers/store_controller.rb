22a0b3b8b17582e8c10c0c81984b220a31af6173
Andrew Benz <andorbal@gmail.com>
2012-04-22 09:42:43 -0500

    Added the cart listing to the application layout so it is viewable everywhere.

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -3,5 +3,6 @@ class StoreController < ApplicationController
     @access_count = access_count
     increment_access_count
     @products = Product.order(:title)
+    @cart = current_cart
   end
 end
